276d6b3930ef59b4929fbb9e3ba06dac526500c5
Charlotte <charlottereadsz@gmail.com>
2026-06-14 23:43:18 +0200

    Fix prompt import path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import Groq from "groq-sdk";
-import { masterPrompt } from "./prompts/temp.js";
+import { masterPrompt } from "./prompts/masterPrompt.js";
 
 console.log(masterPrompt);
 

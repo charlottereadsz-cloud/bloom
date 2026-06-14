41d8f631cd1275d6451d896e8987982dec445de9
Charlotte <charlottereadsz@gmail.com>
2026-06-14 23:35:11 +0200

    Temporary rename

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import Groq from "groq-sdk";
-import { masterPrompt } from "./prompts/masterPrompt.js";
+import { masterPrompt } from "./prompts/temp.js";
 
 console.log(masterPrompt);
 

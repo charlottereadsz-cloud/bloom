9c8e3fe18c5b6bb1296fb0aeb5963befc6bab687
Charlotte <charlottereadsz@gmail.com>
2026-06-15 16:17:03 +0200

    use backend spotify login

--- src/services/spotifyApi.js
+++ src/services/spotifyApi.js
@@ -1,22 +1,4 @@
-const CLIENT_ID = "408e6f6295814270a49adbab9bed6503";
-
-const REDIRECT_URI =
-  "http://127.0.0.1:5173/callback";
-
-const SCOPES = [
-  "playlist-modify-public",
-  "playlist-modify-private",
-];
-
 export function loginWithSpotify() {
-  const authUrl =
-    "https://accounts.spotify.com/authorize?" +
-    new URLSearchParams({
-      client_id: CLIENT_ID,
-      response_type: "token",
-      redirect_uri: REDIRECT_URI,
-      scope: SCOPES.join(" "),
-    });
-
-  window.location.href = authUrl;
+  window.location.href =
+    "https://bloom-api-yd4r.onrender.com/api/spotify/login";
 }
